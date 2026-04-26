d70831a15a3f518673f75149c3e9f648a054058e
1m1 <imi@1m1.io>
2026-04-25 19:39:33 -0600

    .

--- src/GOD.jl
+++ src/GOD.jl
@@ -1,1 +1,10 @@
-module GOD end
+module GOD
+
+using LoopOSMainAgent
+using LoopOSLogging # DEBUG
+
+LoopOSMainAgent.start(name=ARGS[1])
+LoopOSMainAgent.LoopOSAgentManagement.createagent(name="Dona", pkgs=["LoopOSMainAgentAdvice"])
+LoopOSMainAgent.LoopOSAgentManagement.createagent(name="Janet")
+
+end
